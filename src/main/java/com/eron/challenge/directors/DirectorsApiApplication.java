29f6c66762f7d55cbd7a0569f62219439ca2032a
Javier Mayer <jmayer@reba.com.ar>
2025-11-15 13:05:37 -0300

    proyecto base

---
+++ src/main/java/com/eron/challenge/directors/DirectorsApiApplication.java
@@ -0,0 +1,13 @@
+package com.eron.challenge.directors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectorsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectorsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eron/challenge/directors/DirectorsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eron.challenge.directors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectorsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
